3fb285e8cd2b8eaa5500c9715761cae376f79ec4
Jarreth Santos <santos.jarreth@gmail.com>
2024-03-05 09:16:50 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(resultsArray) {
+    const winObject = resultsArray.find(function(obj) {
+        return obj.result === "W";
+    });
+
+    // Check if winObject is defined and return the corresponding year, otherwise return undefined
+    if (winObject) {
+        return winObject.year;
+    } else {
+        return undefined;
+    }
+}
